619b06f9b289d74eae655ae4aff652eb5980f4aa
raul monroy <raulisai97@gmail.com>
2025-04-08 22:52:48 -0600

    Remove obsolete server files and clean up the output directory by deleting internal.js, manifest-full.js, manifest.js, and node files (0.js to 5.js) that are no longer needed.

--- output/client/_app/immutable/chunks/C3BAvKjv.js
+++
@@ -1,1 +0,0 @@
-import{j as a}from"./DCWnqz8X.js";a();
--- output/server/nodes/1.js
+++
@@ -1,8 +0,0 @@
-
-
-export const index = 1;
-let component_cache;
-export const component = async () => component_cache ??= (await import('../entries/fallbacks/error.svelte.js')).default;
-export const imports = ["_app/immutable/nodes/1.CoS22yPO.js","_app/immutable/chunks/HblrPNlH.js","_app/immutable/chunks/DCWnqz8X.js","_app/immutable/chunks/C3BAvKjv.js","_app/immutable/chunks/D1u628wu.js","_app/immutable/chunks/jQwxT0nd.js","_app/immutable/chunks/D741-kNp.js","_app/immutable/chunks/Ck7i__z5.js","_app/immutable/chunks/OI41P7y3.js"];
-export const stylesheets = [];
-export const fonts = [];
